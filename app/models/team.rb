736eb45b9e794a986526c06a8f567c96da325ad0
Nathan Stuart <nathan.stuart01@gmail.com>
2017-05-03 18:17:24 -0600

    added case insensitive search to team database

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,11 @@
 class Team < ApplicationRecord
 
   def self.search(search)
+
     if search
-      where(["name LIKE ?","%#{search}%"])
+      where(["name ILIKE ?","%#{search}%"])
     else
-      where
+      all
     end
   end
 
