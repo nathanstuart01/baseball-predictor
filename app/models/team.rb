5b523636d152c7e94537c9adbc49ff45e4a2d9f5
Nathan Stuart <nathan.stuart01@gmail.com>
2017-05-08 17:35:37 -0600

    added additional model methods to calculating team batting wars, added a team sorter method, team batting war to arry method, converted the batting wars array to float

--- app/models/team.rb
+++ app/models/team.rb
@@ -9,17 +9,29 @@ class Team < ApplicationRecord
     end
   end
 
-teambattingwar = Array.new
 
-  def self.teambattingwar
+
+  def self.teambattingwars
+    require 'csv'
+    teambattingwar = Array.new
     CSV.foreach("teambattingwar.csv", "r:bom|utf-8") do |row|
-    teambattingwar << row[19]
-    updatebattingwar = teambattingwar.drop(1)
+    teambattingwar << row[0,20]
+  end
+
+  def self.team_sorter
+    teams = teambattingwar.map { |el| el[0]}
+  end
+
+  def self.team_batting_war_sorter
+    batting_wars = teambattingwar.map { |el| el[19] }
+  end
 
 
+  def team_batting_wars_to_float
 
-    updatebattingwar.collect |war|
+    batting_wars_to_float = batting_wars.collect do |war|
       war.to_f
+   end
 
       war.inject{ |sum, el| sum + el } /war.size
       possibly puts result here?
