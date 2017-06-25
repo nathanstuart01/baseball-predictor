16874791480f07206ae7b63b8926a58d75a5a0f8
Nathan Stuart <nathan.stuart01@gmail.com>
2017-06-25 11:42:06 -0600

    updated cron job to properly save updated records to db and made cron job run every day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,13 @@
-ENV['RAILS_ENV'] = "development"
+set :environment, "development"
 
-every :sunday, :at => "7:50am" do
+every :day, :at => "08:00am" do
   rake "fetch_batting_stats:collect_batting_stats fetch_batting_stats:save_batting_stats_to_csv
         fetch_batting_stats:turn_batting_csv_into_batting_data fetch_batting_stats:batting_hash_into_db"
 end
 
-every :sunday, :at => "7:50am" do
+set :environment, "development"
+
+every :day, :at => "8:00am" do
   rake "fetch_pitching_stats:collect_pitching_stats fetch_pitching_stats:save_pitching_stats_to_csv
         fetch_pitching_stats:turn_pitching_csv_into_pitching_data fetch_pitching_stats:pitching_hash_into_db"
 end
