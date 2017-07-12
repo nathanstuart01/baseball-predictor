35003c022be403e51a8287ae97548f0ab87be489
Nathan Stuart <nathan.stuart01@gmail.com>
2017-07-11 20:19:12 -0600

    began to add calcualting war functions in js file and teams war index views, not fully working yet

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -1,4 +1,10 @@
 
-function warCalculator() {
-  document.getElementById("game_predictor").innerHTML = "The winner is...";
+
+
+function totalWarCalculator() {
+  var batting = document.getElementById("battingWar").value;
+  var pitching = document.getElementById("pitchingWar").value;
+  var result = (battingWar + pitchingWar);
+
+  document.getElementById("totalWar").innerHTML = 'Total War:' + result;
 }
