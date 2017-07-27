516cf560f379730cb830f410e6e2c77b6728c02f
Nathan Stuart <nathan.stuart01@gmail.com>
2017-07-26 22:11:22 -0600

    started to add ajax calls in js file, json rendering in controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,10 +2,11 @@ class TeamsController < ApplicationController
 
   def index
     @teams = Team.search(params[:search])
+    @team_data = Team.all
+    render json: @team_data
   end
 
   def game_info
-    #@games = JSON.parse(Team.scores)
     @total_war_difference = Team.win_predictor(Team.second.total_war, Team.third.total_war)
   end
 
