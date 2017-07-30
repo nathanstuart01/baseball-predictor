180e63f97e09e8d0ba19d66099c3a5fcfd036e02
Nathan Stuart <nathan.stuart01@gmail.com>
2017-07-30 16:59:37 -0600

    updated js file to include modal, updated stylesheet to include modal, updated games view to include data loading after initial page load

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -1,4 +1,6 @@
 $(document).ready(function (){
+  var gameInfo;
+  var teamData;
 
       $.ajax({
       url: "/games",
@@ -6,8 +8,7 @@ $(document).ready(function (){
       dataType: "JSON",
       data: {}
    }).done(function(games) {
-       var gameInfo = games;
-   //  $(".info").append(teamData[0].name, teamData[0].total_war);
+       gameInfo = games;
        console.log(gameInfo[0].away_team, gameInfo[0].home_team);
        }).fail(function(data) {
        console.log("Data Did Not Load");
@@ -19,10 +20,48 @@ $(document).ready(function (){
     dataType: "JSON",
     data: {}
     }).done(function(teams) {
-      var teamData = teams;
-    // $(".war").append(teamData[0].name);
-    console.log(teamData[0].name, teamData[0].total_war);
+      teamData = teams;
+      console.log(teamData[0].name, teamData[0].total_war);
       }).fail(function(data) {
         console.log("Data Did Not Load");
       });
+
+      function warCalculator(totalWar1, totalWar2) {
+        var rawWinDifference = totalWar1 - totalWar2
+        var totalWarDifference
+
+          if (rawWinDifference < 0) {
+            totalWarDifference = rawWinDifference * -1
+          } else {
+            totalWarDifference = rawWinDifference * 1
+          }
+        document.getElementsByClassName("war").innerHTML = totalWarDifference;
+      };
+
+        // Get the modal
+var modal = document.getElementById('warResults');
+
+// Get the button that opens the modal
+var btn = document.getElementById("warCalculationButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeModal")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
   });
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,7 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.search(params[:search])
     respond_to do |format|
-      format.html 
+      format.html
       format.json { render json: @teams}
       end
   end
