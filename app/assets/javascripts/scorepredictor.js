999ef026e927e393c4b6a1689a124e161eac6996
Nathan Stuart <nathan.stuart01@gmail.com>
2017-08-01 22:04:37 -0600

    updated game, stats info, eliminated team style sheet, got rid of modal button, started to insert query into games controller to only display games that were updated at today, added function to js file to calculate and display war score for just two static teams

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -2,6 +2,19 @@ $(document).ready(function (){
   var gameInfo;
   var teamData;
 
+
+  function warCalculator(totalWar1, totalWar2) {
+    var rawWinDifference = totalWar1 - totalWar2;
+    var totalWarDifference;
+
+        if (rawWinDifference < 0) {
+              totalWarDifference = rawWinDifference * -1
+        } else {
+              totalWarDifference = rawWinDifference * 1
+        }
+          $(".war").append(totalWarDifference);
+        };
+
       $.ajax({
       url: "/games",
       type: "GET",
@@ -9,7 +22,7 @@ $(document).ready(function (){
       data: {}
    }).done(function(games) {
        gameInfo = games;
-       console.log(gameInfo[0].away_team, gameInfo[0].home_team);
+       console.log(gameInfo);
        }).fail(function(data) {
        console.log("Data Did Not Load");
      });
@@ -21,47 +34,10 @@ $(document).ready(function (){
     data: {}
     }).done(function(teams) {
       teamData = teams;
-      console.log(teamData[0].name, teamData[0].total_war);
+      warCalculator(teamData[2].total_war, teamData[3].total_war)
+      console.log(teamData);
       }).fail(function(data) {
         console.log("Data Did Not Load");
       });
-
-      function warCalculator(totalWar1, totalWar2) {
-        var rawWinDifference = totalWar1 - totalWar2
-        var totalWarDifference
-
-          if (rawWinDifference < 0) {
-            totalWarDifference = rawWinDifference * -1
-          } else {
-            totalWarDifference = rawWinDifference * 1
-          }
-        document.getElementsByClassName("war").innerHTML = totalWarDifference;
-      };
-
-        // Get the modal
-var modal = document.getElementById('warResults');
-
-// Get the button that opens the modal
-var btn = document.getElementById("warCalculationButton");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeModal")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
   });
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.where.not(home_team: nil, away_team: nil )
+    @games = Game.where.not(home_team: nil, away_team: nil)
     respond_to do |format|
       format.html
       format.json { render json: @games}
