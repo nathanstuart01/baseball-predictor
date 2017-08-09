ec2fd3e3a71a2c6b87b4096f764e50f5d4d65491
Nathan Stuart <nathan.stuart01@gmail.com>
2017-08-08 23:21:13 -0600

    added more js functions for game data

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -3,6 +3,8 @@ $(document).ready(function (){
   var teamData;
   var rawWinDifference;
   var totalWarDifference;
+  var gamesData = [];
+  var awayData =[];
 
       $.ajax({
         url: "/games",
@@ -24,9 +26,9 @@ $(document).ready(function (){
     }).done(function(teams) {
         teamData = teams;
         console.log(teamData);
-
         awayTeamsMatcher();
         homeTeamsMatcher();
+        gamesDataCreator();
     }).fail(function(data) {
         console.log("Data Did Not Load");
       });
@@ -44,7 +46,6 @@ $(document).ready(function (){
     function awayTeamsMatcher() {
       var awayTeams = teamData;
       var awayGames = gameInfo;
-      var awayData = [];
       var x;
       var y;
       var i;
@@ -54,7 +55,6 @@ $(document).ready(function (){
           for (i = 0, l = awayTeams.length; i < l; i++) {
             if ( awayTeams[i].name === awayGames[x].away_team ) {
                 awayData.push(awayGames[x].id, awayTeams[i].name, awayTeams[i].total_war);
-                console.log(awayData);
              }
           }
         }
@@ -63,7 +63,7 @@ $(document).ready(function (){
     function homeTeamsMatcher() {
       var homeTeams = teamData;
       var homeGames = gameInfo;
-      var homeData = [];
+      var homeData =[];
       var a;
       var b;
       var c;
@@ -73,10 +73,13 @@ $(document).ready(function (){
           for (c = 0, d = homeTeams.length; c < d; c++) {
             if ( homeTeams[c].name === homeGames[a].home_team ) {
                 homeData.push(homeGames[a].id, homeTeams[c].name, homeTeams[c].total_war);
-                console.log(homeData);
              }
           }
         }
+
+    function gamesDataCreator() {
+      console.log(awayData);
+    };
 
     };
 
