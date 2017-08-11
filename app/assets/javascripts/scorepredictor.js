744bd176c94d0566075e3afcb46b0f840730d0a6
Nathan Stuart <nathan.stuart01@gmail.com>
2017-08-10 23:35:10 -0600

    added js callback functions and logic within functions being called back to not load data until previous data was complete, updated stats

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -1,10 +1,12 @@
 $(document).ready(function (){
+
   var gameInfo;
   var teamData;
   var rawWinDifference;
   var totalWarDifference;
   var gamesData = [];
   var awayData =[];
+  var homeData =[];
 
       $.ajax({
         url: "/games",
@@ -26,67 +28,66 @@ $(document).ready(function (){
     }).done(function(teams) {
         teamData = teams;
         console.log(teamData);
-        awayTeamsMatcher();
-        homeTeamsMatcher();
-        gamesDataCreator();
+        if (teamData.length === 30) {
+          awayTeamsMatcher(homeTeamsMatcher);
+        } else {
+          alert("Data Loading...");
+        }
     }).fail(function(data) {
         console.log("Data Did Not Load");
       });
 
-      function warCalculator(totalWar1, totalWar2) {
-        rawWinDifference = totalWar1 - totalWar2;
+      function awayTeamsMatcher(callback) {
+          var awayTeams = teamData;
+          var awayGames = gameInfo;
+          var x;
+          var y;
+          var i;
+          var l;
 
-          if (rawWinDifference < 0) {
-            totalWarDifference = rawWinDifference * -1
-          } else {
-            totalWarDifference = rawWinDifference * 1
-          }
-        };
+            for (x = 0, y = awayGames.length; x < y; x ++) {
+              for (i = 0, l = awayTeams.length; i < l; i++) {
+                if ( awayTeams[i].name === awayGames[x].away_team ) {
+                    awayData.push(awayGames[x].id, awayTeams[i].name, awayTeams[i].total_war);
+                    console.log(awayData);
+                 }
+              }
+            }
 
-    function awayTeamsMatcher() {
-      var awayTeams = teamData;
-      var awayGames = gameInfo;
-      var x;
-      var y;
-      var i;
-      var l;
-
-        for (x = 0, y = awayGames.length; x < y; x ++) {
-          for (i = 0, l = awayTeams.length; i < l; i++) {
-            if ( awayTeams[i].name === awayGames[x].away_team ) {
-                awayData.push(awayGames[x].id, awayTeams[i].name, awayTeams[i].total_war);
-             }
-          }
-        }
-    };
-
-    function homeTeamsMatcher() {
-      var homeTeams = teamData;
-      var homeGames = gameInfo;
-      var homeData =[];
-      var a;
-      var b;
-      var c;
-      var d;
-
-        for (a = 0, b =homeGames.length; a < b; a ++) {
-          for (c = 0, d = homeTeams.length; c < d; c++) {
-            if ( homeTeams[c].name === homeGames[a].home_team ) {
-                homeData.push(homeGames[a].id, homeTeams[c].name, homeTeams[c].total_war);
-             }
-          }
+            if(typeof callback === "function") {
+              callback();
+            }
         }
 
-    function gamesDataCreator() {
-      console.log(awayData);
-    };
 
-    };
+        function homeTeamsMatcher() {
+          var homeTeams = teamData;
+          var homeGames = gameInfo;
+          var a;
+          var b;
+          var c;
+          var d;
 
-    // need to take the data that I have parsed in my console, somehow pass it to the function warcalculator
-    // need to assign a variable value to each away and home team value
-    // is there a game to pass things to war calculator based on game id
-    // push results to an away and home team game array??
-    //let the function warcalculator calculate the values and thne spit it out onto the list items for each individual game
+            for (a = 0, b =homeGames.length; a < b; a ++) {
+              for (c = 0, d = homeTeams.length; c < d; c++) {
+                if ( homeTeams[c].name === homeGames[a].home_team ) {
+                    homeData.push(homeGames[a].id, homeTeams[c].name, homeTeams[c].total_war);
+                    console.log(homeData);
+                 }
+              }
+            }
+            if (homeData.length === awayData.length) {
+                pushDataIntoGamesArray(awayData);
+            } else {
+              alert("Game data loading...");
+            }
+          }
+
+        function pushDataIntoGamesArray(arr1) {
+          for (var e = 0, len = 1; e < len; e++) {
+            gamesData.push(awayData);
+          }
+          console.log(awayData);
+        }
 
   });
