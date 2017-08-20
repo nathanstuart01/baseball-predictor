5c97a75efa32f1f1208cc105bd90087706b8e3f2
Nathan Stuart <nathan.stuart01@gmail.com>
2017-08-20 16:49:13 -0600

    added more js stuff to calculate predicted winner

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -2,7 +2,6 @@ $(document).ready(function (){
 
   var gameInfo;
   var teamData;
-  var awayData =[];
   var gamesData = [];
 
       $.ajax({
@@ -26,7 +25,7 @@ $(document).ready(function (){
         teamData = teams;
         console.log(teamData);
         if (teamData.length === 30) {
-          awayTeamsMatcher(allGames);
+          teamsMatcher(allGames);
         } else {
           alert("Data Loading...");
         }
@@ -34,19 +33,18 @@ $(document).ready(function (){
         console.log("Data Did Not Load");
       });
 
-      function awayTeamsMatcher(callback) {
-          var awayTeams = teamData;
-          var awayGames = gameInfo;
+      function teamsMatcher(callback) {
+          var teams = teamData;
+          var games = gameInfo;
           var x;
-          var y;
           var i;
-          var l;
 
-            for (x = 0, y = awayGames.length; x < y; x ++) {
-              for (i = 0, l = awayTeams.length; i < l; i++) {
-                if ( awayTeams[i].name === awayGames[x].away_team || awayTeams[i].name === awayGames[x].home_team ) {
-                    awayData.push(awayGames[x].id, awayTeams[i].name, awayTeams[i].total_war);
-                    console.log(awayData);
+
+            for (x = 0; x < games.length; x ++) {
+              for (i = 0; i < teams.length; i++) {
+                if ( teams[i].name === games[x].away_team || teams[i].name === games[x].home_team ) {
+                    gamesData.push(teams[i].name, teams[i].total_war);
+                    console.log(gamesData);
                  }
               }
             }
@@ -56,35 +54,21 @@ $(document).ready(function (){
             }
         }
 
-        function Games(gameNumber, awayTeam, awayWar, homeTeam, homeWar) {
-            this.gameNumber = gameNumber;
-            this.awayTeam = awayTeam;
-            this.awayWar = awayWar;
-            this.homeTeam = homeTeam;
-            this.homeWar = homeWar;
+        function Games(teamOne, teamOneWar, teamTwo, teamTwoWar) {
+            this.teamOne = teamOne;
+            this.teamOneWar = teamOneWar;
+            this.teamTwo = teamTwo;
+            this.teamTwoWar = teamTwoWar;
         }
 
-    //    var gameOne = new Games(1, "Mariners", 16.7, "Orioles", 20.0);
-    //    var gameTwo = new Games(2, "yankees", 16.7, "rangers", 20.0);
 
         function allGames() {
-          var z;
-          // create dynamic objects, see stackoverflow, by looping through games and creating a new object for each game there is
-          // loop through all games data and add the data to each game object and find a way to only add game number once,
-          // once all objects have a value, go to the next object and start looping through that object as well
-          // continue filling up objects, only adding one new game number, and then starting to fill up the new objects once all existing objects properties are not null
-          // look at ways to check properitie of an object in js
-          // maybe do a sort, or type of
-          // maybe use a regexp to eliminate duplicate non-float numbers 
-          for ( z = 0; z < awayData.length; z ++) {
-            if (awayData % 3 === 0) {
-            gamesData.push(awayData)
-            console.log(gamesData)
-          }
-          }
-      //    gamesData.push(gameOne, gameTwo)
-      //    console.log(awayData);
-      //    console.log(gamesData);
+          var gamesDataz = [];
+          var game[x];
+
+          for
+          gamesDataz.push(gameOne1, gameTwo2);
+          console.log(gamesDataz[0]["teamTwoWar"]);
 
         }
 
