7ca09fe131a7f728983e6262763d73c055633b24
Nathan Stuart <nathan.stuart01@gmail.com>
2017-09-30 14:44:50 -0600

    added additional functions that display game data and predicted winners in inline block divs, added divs and titls within html display page to properly display desired data, added css properties to display divs side by side

--- app/assets/javascripts/scorepredictor.js
+++ app/assets/javascripts/scorepredictor.js
@@ -71,54 +71,68 @@ $(document).ready(function (){
          console.log(allGamesData);
          gameDisplayer();
      }
+          function gameDisplayer() {
+           for (let a = 0; a < gameInfo.length; a ++ ) {
+             let gameInfoAwayTeam = gameInfo[a]["away_team"];
+              let gameInfoHomeTeam = gameInfo[a]["home_team"];
+              let gameInfoTime = gameInfo[a]["game_info"];
 
-      function gameDisplayer() {
+              let currentDiv = document.getElementById("gameInformation2");
+              let newUlElement = document.createElement("ul");
+              let newContent = document.createTextNode(gameInfoAwayTeam + ' @ ' + gameInfoHomeTeam);
+              currentDiv.appendChild(newUlElement);
+              newUlElement.append(newContent);
 
-
-
-       for (var a = 0; a < gameInfo.length; a ++ ) {
-         var gameInfoAwayTeam = gameInfo[a]["away_team"];
-          var gameInfoHomeTeam = gameInfo[a]["home_team"];
-          var gameInfoTime = gameInfo[a]["game_info"];
-
-          var currentDiv = document.getElementById("gameInformation");
-          var newTdElement = document.createElement("td");
-          var newContent = document.createTextNode(gameInfoAwayTeam + '@' + gameInfoHomeTeam);
-          currentDiv.appendChild(newTdElement);
-          newTdElement.append(newContent);
-
-          var currentDiv2 = document.getElementById("firstPitchInformation");
-          var newTdElement2 = document.createElement("td");
-          var newContent2 = document.createTextNode(new Date(gameInfoTime).toLocaleTimeString());
-          currentDiv2.appendChild(newTdElement2);
-          newTdElement2.append(newContent2);
+              let currentDiv2 = document.getElementById("firstPitchInformation2");
+              let newUlElement2 = document.createElement("ul");
+              let newContent2 = document.createTextNode(new Date(gameInfoTime).toLocaleTimeString());
+              currentDiv2.appendChild(newUlElement2);
+              newUlElement2.append(newContent2);
+                  }
+              gameWinnerPredictor();
               }
+
+          function gameWinnerPredictor() {
+            for (let a = 0; a < allGamesData.length; a ++) {
+              let team1 = allGamesData[a]["teamOne"];
+              let team2 = allGamesData[a]["teamTwo"];
+              let neitherTeam ='Push';
+              let team1War = allGamesData[a]["teamOneWar"];
+              let team2War = allGamesData[a]["teamTwoWar"];
+              let currentDiv3 = document.getElementById("predictedWinner2");
+              let newUlElement3 = document.createElement("ul");
+              let newContent3 = document.createTextNode(team1);
+              let newContent4 = document.createTextNode(team2);
+              let newContent5 = document.createTextNode(team2);
+
+              if (team1War > team2War) {
+                currentDiv3.appendChild(newUlElement3);
+                newUlElement3.append(newContent3);
+              } if ( team1War === team2War) {
+                currentDiv3.appendChild(newUlElement3);
+                newUlElement3.append(neitherTeam);
+              }
+              else {
+                currentDiv3.appendChild(newUlElement3);
+                newUlElement3.append(newContent4);
+              }
+            }
+
+            winCalculator();
           }
 
-
-      function warCalculator() {
-        for (var b = 0; b < allGamesData.length; b ++) {
-          var teamInfo1 = allGamesData[b]["teamOne"];
-          var teamInfo2 = allGamesData[b]["teamTwo"];
-          var teamWar1 = allGamesData[b]["teamOneWar"];
-          var teamWar2 = allGamesData[b]["teamTwoWar"];
-          var p = $('.gameInformation');
-        document.body.appendChild('<li>' + teamInfo1 + '</li>');
-
-        }
-      }
-
-
-
-        //each game gets the x value from the loop
-        // take team 1 war and subtract team 2 war from it
-        // if result is less than 0, multiply result by -1
-        // take result and send it to a case statement,
-        // each result will be a different variable tied to the game
-        // results will change, 0-5 difference, 55% chance of winning, 6-10, 60% chance of winning, 11-20 65% chance of winning, 21-25 70% chance of winning or better, 26+ 75% or greater chance of winning
-        // append the text statement to another list item showing eachs win probability
-
-
-
-
+          function winCalculator() {
+            //setup something simliar to gameWinnerPredictor
+            // assign values and based on the difference betweeen war scores, append the different variable values
+            // let percent1 = 'Even, if wars equal%'
+            // let percent2 = '55%, if war only 1-5 greater'
+            // let percent3 = '60%, if war only 6-10 greater'
+            // let percent4 = '65%, if war only 11-15 greater'
+            // let percent5 = '70%, if war only 16-20 greater'
+            // let percent6 = '75%, if war only 21-25 greater'
+            // let percent7 = '80%, if war only 26-30 greater'
+            // let percent8 = '85%, if war only 31-35 greater'
+            // let percent9 = '90%, if war only 36-40 greater'
+            // let percent10 = '99%, if war only 41 or greater'
+          }
   });
