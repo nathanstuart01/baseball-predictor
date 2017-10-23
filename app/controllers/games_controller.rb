b40ff728a9d5684b7ccf8b9c17b11d2b409239b8
Nathan Stuart <nathan.stuart01@gmail.com>
2017-10-23 16:29:27 -0600

    completed css styles to make app mobile responsive

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.where("updated_at >= ?", Time.zone.now.beginning_of_day).where.not(home_team: nil, away_team: nil).order(game_info: :asc)
+    #@games = Game.where("updated_at >= ?", Time.zone.now.beginning_of_day).where.not(home_team: nil, away_team: nil).order(game_info: :asc)
+    @games = Game.where.not(home_team: nil, away_team: nil).order(game_info: :asc)
     respond_to do |format|
       format.html
       format.json { render json: @games}
